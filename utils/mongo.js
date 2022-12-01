0dcb88465993cd5e291d8904bf232f53ec5c9c2b
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-12-01 14:01:25 +0100

    change security config to versel deployment

---
+++ utils/mongo.js
@@ -0,0 +1,9 @@
+const USERNAME = 'react-course';
+const PASSWORD = '***REMOVED***';
+
+export const DB_URL =
+	'mongodb+srv://' +
+	USERNAME +
+	':' +
+	PASSWORD +
+	'@cluster0.hpxjkxe.mongodb.net/?retryWrites=true&w=majority';
