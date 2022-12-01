b2fe772bae573ae03d3194982d555d98d3b2e75d
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-12-01 10:49:53 +0100

    add meetupDetails page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetails from '../../components/meetups/MeetupDetails';
+
+function MeetupDetailsPage() {
+	return (
+		<MeetupDetails
+			image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+			title="First Meetup"
+			adress="Here"
+			description="description"
+		/>
+	);
+}
+
+export default MeetupDetailsPage;
