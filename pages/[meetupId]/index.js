055f445b16aa00396d26b3934d4b6ea43fb8b61b
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-12-01 11:26:25 +0100

    use getStaticPros in dynamic pages (need getStaticPath)

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ function MeetupDetailsPage() {
 	);
 }
 
+export async function getStaticPaths() {
+	return {
+		//should be write programmatically
+		//fallback indicated if they are other path or not
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: 'm1',
+				},
+			},
+			{
+				params: {
+					meetupId: 'm2',
+				},
+			},
+		],
+	};
+}
+
+export async function getStaticProps(context) {
+	// fetch data for a single meetup
+
+	const meetupId = context.params.meetupId;
+
+	return {
+		props: {
+			meetupData: {
+				image:
+					'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+				id: meetupId,
+				title: 'First Meetup',
+				address: 'Some Street 5, Some City',
+				description: 'This is a first meetup',
+			},
+		},
+	};
+}
+
 export default MeetupDetailsPage;
