5fdf1cd1c0ae1feac22f827d81b69b3777e794b0
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-12-01 11:06:47 +0100

    use getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,40 @@
+import {useEffect, useState} from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
-function HomePage() {
-	const DUMMY_MEETUPS = [
-		{
-			id: 'm1',
-			title: 'A First Meetup',
-			image:
-				'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-			address: 'Some address 5, 12345 Some City',
-			description: 'This is a first meetup!',
+const DUMMY_MEETUPS = [
+	{
+		id: 'm1',
+		title: 'A First Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Some address 5, 12345 Some City',
+		description: 'This is a first meetup!',
+	},
+	{
+		id: 'm2',
+		title: 'A Second Meetup',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+		address: 'Some address 10, 12345 Some City',
+		description: 'This is a second meetup!',
+	},
+];
+
+function HomePage(props) {
+	const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+	useEffect(() => {
+		setLoadedMeetups(DUMMY_MEETUPS);
+	}, []);
+	return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+	return {
+		props: {
+			meetups: DUMMY_MEETUPS,
 		},
-		{
-			id: 'm2',
-			title: 'A Second Meetup',
-			image:
-				'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-			address: 'Some address 10, 12345 Some City',
-			description: 'This is a second meetup!',
-		},
-	];
-	return <MeetupList meetups={DUMMY_MEETUPS} />;
+	};
 }
 
 export default HomePage;
